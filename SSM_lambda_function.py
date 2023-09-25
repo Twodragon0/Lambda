c09000d32353f2a810b6ad1fb82657054f704162
Twodragon <33538534+Twodragon0@users.noreply.github.com>
2023-09-25 16:27:13 +0900

    Update SSM_lambda_function.py

--- SSM_lambda_function.py
+++ SSM_lambda_function.py
@@ -16,48 +16,41 @@ def consoleUrlReturn(awsRegion, eventID):
     consoleUrl = url + awsRegion + "#/events?EventId=" + eventID
     return consoleUrl
 
-def handle_session_manager_event(event):
+def lambda_handler(event, context):
     # Extract relevant information from the Session Manager event
-    session_id = event['responseElements']['sessionId']
-#    target_instance_id = event['requestParameters']['target']
-    event_time = returnTime(event['eventTime'])
-    event_name = event['eventName']
-    awsRegion = event["awsRegion"]
-    eventID = event["eventID"]
+    session_id = event['detail']['responseElements']['sessionId']
+    # target_instance_id = event['detail']['requestParameters']['target']
+    event_time = returnTime(event['detail']['eventTime'])
+    event_name = event['detail']['eventName']
+    awsRegion = event['detail']["awsRegion"]
+    eventID = event['detail']["eventID"]
     consoleUrl = consoleUrlReturn(awsRegion, eventID)
 
+    webhook_url = "*"
 
-    # Check for 'StartSession', 'ResumeSession', and 'TerminateSession' events
-    if event_name in ["StartSession", "ResumeSession", "TerminateSession"]:
-        # Customize the message content for Session Manager events
-        slack_data = {
-            "attachments": [
-                {
-                    "pretext": "*Session Manager Event*",
-                    "title" : "RawData",
-                    "title_link" : consoleUrl,
-                    "fields": [
-                        {"title": f"Session ID: {session_id}"},
-                        {"title": "Event Time (KST)", "value": event_time, "short": True},
-#                        {"title": "Target Instance ID", "value": target_instance_id, "short": True},
-                        {"title": "Event Name", "value": event_name, "short": True},
-                    ],
-                    "color": "#00ffff",  # Default color
-                }
-            ]
-        }
+    slack_data = {
+        "attachments": [
+            {
+                "pretext": "*Session Manager Event*",
+                "title": "RawData",
+                "title_link": consoleUrl,
+                "fields": [
+                    {"title": f"Session ID: {session_id}"},
+                    {"title": "Event Time (KST)", "value": event_time, "short": True},
+                    # {"title": "Target Instance ID", "value": target_instance_id, "short": True},
+                    {"title": "Event Name", "value": event_name, "short": True},
+                ],
+                "color": "#00ffff",  # Default color
+            }
+        ]
+    }
 
-        webhook_url = "*"
-
-        response = requests.post(
-            webhook_url, data=json.dumps(slack_data),
-            headers={'Content-Type': 'application/json'}
+    response = requests.post(
+        webhook_url, data=json.dumps(slack_data),
+        headers={'Content-Type': 'application/json'}
+    )
+    if response.status_code != 200:
+        raise ValueError(
+            'Request to Slack returned an error %s, the response is:\n%s'
+            % (response.status_code, response.text)
         )
-        if response.status_code != 200:
-            raise ValueError(
-                'Request to Slack returned an error %s, the response is:\n%s'
-                % (response.status_code, response.text)
-            )
-
-def lambda_handler(event, context):
-    handle_session_manager_event(event['detail'])
